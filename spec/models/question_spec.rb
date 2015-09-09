1b376726e01c2bac293a13e4e216af7972d9543e
Martin Soler <martinrsoler@yahoo.com>
2015-09-09 11:46:44 -1000

    Assignment 28, Models-question_spec.rb streamlined

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,17 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  let(:question) { Question.create!(title: "New Question Title", body: "New Question Body", resolved: false) }
-
   describe "attributes" do
-    it "should respond to title" do
-      expect(question).to respond_to(:title)
-    end
-    it "should respond to body" do
-      expect(question).to respond_to(:body)
-    end
-    it "should respond to resolved" do
-      expect(question).to respond_to(:resolved)
-    end
+    it { is_expected.to respond_to(:title) }
+    it { is_expected.to respond_to(:body) }
+    it { is_expected.to respond_to(:resolved) }
   end
 end
