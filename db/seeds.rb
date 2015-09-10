a965d79e7f60eaa93bc72e63afec0a3120228cf8
Martin Soler <martinrsoler@yahoo.com>
2015-09-10 07:59:47 -1000

    Assignment 30, Seed Data completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,17 @@
 include RandomData
-
+ 
 # Create Posts
 50.times do
   Post.create!(
-    title: RandomData.random_sentence,
-    body:  RandomData.random_paragraph
+    title:  RandomData.random_sentence,
+    body:   RandomData.random_paragraph
   )
 end
-posts = Post.all
 
+# Add a unique non-random Post
+p = Post.find_or_create_by(title: 'Non Random Post', body: 'This is a non-random post')
+posts = Post.all
+ 
 # Create Comments
 100.times do
   Comment.create!(
@@ -16,6 +19,8 @@ posts = Post.all
     body: RandomData.random_paragraph
   )
 end
+# Add a unique comment to the non-random Post created
+Comment.find_or_create_by(post: p, body: 'This is a non-random comment')
 
 puts "Seed finished"
 puts "#{Post.count} posts created"
