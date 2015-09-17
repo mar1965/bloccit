7ef45eb0caf96dc89a568f88d5c540e4fec0081f
Martin Soler <martinrsoler@yahoo.com>
2015-09-16 17:39:22 -1000

    Assignment 33, More CRUD completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,14 @@
 include RandomData
 
-# Create Posts
+# Create Questions
 50.times do
-  Post.create!(
+  Question.create!(
     title: RandomData.random_sentence,
-    body:  RandomData.random_paragraph
+    body:  RandomData.random_paragraph,
+    resolved: false
   )
 end
-posts = Post.all
-
-# Create Comments
-100.times do
-  Comment.create!(
-    post: posts.sample,
-    body: RandomData.random_paragraph
-  )
-end
+questions = Question.all
 
 puts "Seed finished"
-puts "#{Post.count} posts created"
-puts "#{Comment.count} comments created"
+puts "#{Question.count} questions created"
