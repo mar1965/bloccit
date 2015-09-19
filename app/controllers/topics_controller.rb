96a8177fb06c1cf3faebeb7dc889c2bb9ac97af6
Martin Soler <martinrsoler@yahoo.com>
2015-09-18 17:31:16 -1000

    Checkpoint 34 completed, Topics and Posts

---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,61 @@
+class TopicsController < ApplicationController
+  def index
+    @topics = Topic.all
+  end
+
+  def show
+    @topic = Topic.find(params[:id])
+  end
+
+  def new
+    @topic = Topic.new
+  end
+
+  def create
+    @topic = Topic.new(topic_params)
+
+    if @topic.save
+      flash[:notice] = "Topic was saved successfully."
+      redirect_to @topic
+    else
+      flash[:error] = "There was an error saving the topic. Please try again."
+      render :new
+    end
+  end
+
+  def topic_params
+    params[:topic].permit(:name, :description, :public)
+  end
+
+  def edit
+    @topic = Topic.find(params[:id])
+  end
+
+  def update
+    @topic = Topic.find(params[:id])
+
+    @topic.name = params[:topic][:name]
+    @topic.description = params[:topic][:description]
+    @topic.public = params[:topic][:public]
+
+    if @topic.save
+      flash[:notice] = "Topic was updated."
+      redirect_to @topic
+    else
+      flash[:error] = "There was an error saving the topic. Please try again."
+      render :edit
+    end
+  end
+
+  def destroy
+    @topic = Topic.find(params[:id])
+
+    if @topic.destroy
+      flash[:notice] = "\"#{@topic.name}\" was deleted successfully."
+      redirect_to action: :index
+    else
+      flash[:error] = "There was an error deleting the topic."
+      render :show
+    end
+  end
+end
