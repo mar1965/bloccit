904eaf3de4422bbe55d42870c98dbd476d98cf36
Martin Soler <martinrsoler@yahoo.com>
2015-10-02 15:55:09 -1000

    Assignment 41, Authentication Signing In completed

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+
+  def avatar_url(user)
+    gravatar_id = Digest::MD5::hexdigest(current_user.email).downcase
+    gravatar_url = "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
+    image_tag(gravatar_url, class: "gravatar")
+  end
+
 end
