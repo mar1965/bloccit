17e2ba325f49ee749dfbf4d0854a4a7fd45f999c
Martin Soler <martinrsoler@yahoo.com>
2016-12-19 16:35:30 -1000

    Checkpoint 45 completed, Favorites

--- app/controllers/labels_controller.rb
+++ app/controllers/labels_controller.rb
@@ -1,5 +1,5 @@
 class LabelsController < ApplicationController
   def show
-    @label = Label.find(param[:id])
+    @label = Label.find(params[:id])
   end
 end
--- app/helpers/labels_helper.rb
+++ app/helpers/labels_helper.rb
@@ -1,5 +1,5 @@
 module LabelsHelper
   def labels_to_buttons(labels)
-    raw labels.map { |l| link_to l.name, label_path(id: l.id), class: 'btn-xs ben-primary' }.join(' ')
+    raw labels.map { |l| link_to l.name, label_path(id: l.id), class: 'btn-xs btn-primary' }.join(' ')
   end
 end
