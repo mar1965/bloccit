c14907d1ed15401dad34c5674c828b238e394358
Martin Soler <martinrsoler@yahoo.com>
2017-01-16 11:03:33 -1000

    Assignment 49 fixed route to nest post under topic; included post in Topic API

--- config/routes.rb
+++ config/routes.rb
@@ -23,8 +23,11 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:index, :show, :create, :update]
-      resources :topics, except: [:edit, :new]
-      resources :posts, except: [:edit, :new]
+      resources :topics, except: [:edit, :new] do
+        resources :posts, only: [:create, :update]
+      end
+      resources :posts, only: [:index, :show, :update, :destroy]
+      resources :comments, only: [:index, :show]
     end
   end
 end
